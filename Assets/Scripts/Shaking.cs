3f52fc52529d2a850325ea3843a5951e1e3d7960
Rafia Bushra <rafia0037@gmail.com>
2017-10-28 15:39:31 -0700

    added action items

---
+++ Assets/Scripts/Shaking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaking : MonoBehaviour {
+
+
+	void OnMouseDown () {
+		Debug.Log ("mouse clicked");
+	}
+		
+	// Use this for initialization
+	void Start () {
+		
+	}
+		
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
