a8ea4ffcfa5649d837eba56c2295ec3cecc2f754
Rory Ksionda <rory.ksionda@mixpanel.com>
2017-10-28 12:49:59 -0700

    Draggable silly picture

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour {
+    private bool clickedOn;
+	
+	void Start () {
+	}
+	
+	void Update () {
+		if (clickedOn) {
+			Dragging ();
+        }
+	}
+	
+	void OnMouseDown () {
+		clickedOn = true;
+	}
+	
+	void OnMouseUp () {
+		clickedOn = false;
+	}
+
+	void Dragging () {
+		Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		
+		mouseWorldPoint.z = 0f;
+		transform.position = mouseWorldPoint;
+	}
+}
