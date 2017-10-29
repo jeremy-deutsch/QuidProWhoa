a0761521d36856a736e9b48c5284afe8984b99e0
Rory Ksionda <rory.ksionda@mixpanel.com>
2017-10-29 11:33:11 -0700

    MOAR ingredients

--- rename.py
+++ rename.py
@@ -1,8 +1,33 @@
 import subprocess
 from glob import glob
 from pprint import pprint
+import os
+import random
+import json
 
 ingredients = glob('Assets/Resources/IngredientSprites/*.png')
 
-pprint(ingredients)
+ingredients = [os.path.basename(x).replace('.png', '') for x in ingredients]
+
+buffs = [
+    "STR",
+    "STA",
+    "INT",
+    "COUR",
+    "CHA",
+    "SPE",
+]
+
+jsonData = [
+]
+for ingredient in ingredients:
+    jsonData.append({
+        "name": ingredient,
+        "buff": random.choice(buffs)
+    })
+
+with open('Assets/JSON/ingredients.json', 'w') as outfile:
+    json.dump({
+        "ingredients": jsonData,
+    }, outfile, indent=4)
 
