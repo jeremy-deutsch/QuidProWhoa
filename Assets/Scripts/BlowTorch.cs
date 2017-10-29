e01ef7050f26b0ac4a0a9441854bcc7ebf8c80f7
Rafia Bushra <rafia0037@gmail.com>
2017-10-28 22:40:39 -0700

    Fixed blow torch draggability

---
+++ Assets/Scripts/BlowTorch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlowTorch : MonoBehaviour {
+
+	private float counter = 0.0f;
+
+	//bool flaming = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnMouseEnter () {
+		Debug.Log ("Entered");
+		if (Input.GetKey (KeyCode.Space)) {
+			Debug.Log ("space is being pressed");
+		}
+
+	}	
+	/*
+	void Update() {
+		if (flaming) {
+			counter += Time.fixedDeltaTime;
+			Debug.Log ("mouse is being held" + counter);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		flaming = true;
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+
+	}*/
+}
